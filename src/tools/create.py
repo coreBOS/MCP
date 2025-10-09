005396e69212806e060e49fc2531759a8a74deb5
Joe Bordes <joe@tsolucio.com>
2025-10-09 18:18:42 +0200

    feat(Invoice) create inventory modules prompts and procedure

--- src/tools/create.py
+++ src/tools/create.py
@@ -6,6 +6,20 @@ def register(mcp: FastMCP):
     async def create_record(entity: str, fields: dict):
         """
         Create a new record in the specified entity with the given fields in your Evolutivo Application.
+        When creating an Invoice, SalesOrder, Quotes, or PurchaseOrder there is a special field value for the inventory lines
+        'pdoInformation': [
+            [
+                "productid": 'product/service ID',
+                "comment": 'some comment',
+                "qty": 1, // number of units to be sold
+                "listprice": 10,
+                'discount': 0,  // 0 no discount, 1 discount
+                "discount_type": 0,  //  amount/percentage
+                "discount_percentage": 0,  // not needed nor used if type is amount
+                "discount_amount": 0,  // not needed nor used if type is percentage
+            ],
+        ]
+
         Args:
             entity (str): entity where the new record will be created
             fields (dict): dictionary with field names and their values for the new record
--- src/tools/prompt_create.py
+++ src/tools/prompt_create.py
@@ -17,3 +17,23 @@ def register(mcp: FastMCP):
             f"Create a new {entity} record with the provided values using the create tool",
             f"Return the ID of the newly created record or an error message if the creation failed",
         ]
+
+    @mcp.prompt()
+    def create_inventory_procedure(entity: str) -> list[str]:
+        """
+        Defines the steps to create an Invoice, SalesOrder, Quotes or PurchaseOrder record.
+        """
+        return [
+            f"Use get_fields metainformation to get a list of fields for the {entity} entity",
+            f"Loop over all the mandatory fields asking the user for values. Use the metainformation about field types to show possible values for each field",
+            f"Ask if the user wants to set any optional fields. If yes, loop over all the optional fields asking the user for values. Use the metainformation about field types to show possible values for each field",
+            f"Validate each field with it's type",
+            f"Ask the user for the inventory lines details by doing a loop that asks for:"
+            f"Product or Service ID"
+            f"Ask for the Quantity of the inventory line"
+            f"Use the product or service ID to retrieve the price"
+            f"Ask if they want to modify the Price"
+            f"Ask if there is any Discount to apply to the inventory line"
+            f"Create a new {entity} record with the provided values using the create tool with the special pdoInformation array",
+            f"Return the ID of the newly created record or an error message if the creation failed",
+        ]
